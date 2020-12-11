c4be8340ad87535ac0b199bd9ebf72992115b4ab
410AngelaVu <andjela1993@gmail.com>
2020-12-11 17:18:13 +0100

    Added methods in the Post Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,45 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:new, :create]
+  
+   def index
+    @posts = Post.all.order('created_at DESC')
+   end
+
+
   def new
     @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
+    if @post.save
+     redirect_to @post
+   else
+    render 'new'
+  end
+  end
+
+  def show
+   @post = Post.find(params[:id])
+  end
+
+  def edit
+  @post = Post.find(params[:id])
+  end
+
+  def update
+   @post = Post.find(post[:id])
+   if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+  end
+
+  def destroy 
+   @post = Post.find(params[:id])
+   @post.destroy
+   redirect_to root_path
   end
 
   private
