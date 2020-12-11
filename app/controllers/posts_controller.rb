f9f1ae44bb53e5f91889cd0d09943572b1a14b4d
410AngelaVu <andjela1993@gmail.com>
2020-12-11 23:12:33 +0100

    Added style to the index page and make visible links to the logged in user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,45 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :create]
-  def new
+  # before_action :authenticate_user!, except: [:new, :create]
+  def index
+   @posts = Post.all.order('created_at DESC')
     @post = Post.new
   end
 
   def create
     @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def new
     @post = current_user.posts.build
   end 
+
+  def show
+   @post = Post.find(params[:id])
+  end
+
+  def edit
+  @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy 
+   @post = Post.find(params[:id])
+   @post.destroy
+   redirect_to root_path
+  end
     
   private
   def post_params
